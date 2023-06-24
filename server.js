d248294ecd52ffd3e8ddebc9857ac816adab58c7
Pragati Prasanna <pragatiprasanna@Pragatis-MBP.bbrouter>
2023-06-24 07:48:04 +0530

    commit

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ const connectDB = async () => {
 
       mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser : true,
-        UseUnifiedTopology: true
+        useUnifiedTopology: true
       });
 
       console.log('MongoDB is connected');
